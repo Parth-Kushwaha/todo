1ba2721c6523ab141b5f9b54677ff70459495e6a
Parth-Kushwaha <parthkushwaha2003@gmail.com>
2023-08-28 14:12:32 +0530

    user admin model

--- authentication/models.py
+++ authentication/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    profile_photo=models.ImageField(null=True)
+    profile_photo=models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.username
--- tasklist/admin.py
+++ tasklist/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import TaskList
 
 # Register your models here.
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display=['user']
+
+admin.site.register(TaskList, TaskAdmin)
