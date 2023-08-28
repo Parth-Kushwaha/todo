02d60049b3a16aadff9fcf2aeaeef6c8001376d0
Parth-Kushwaha <parthkushwaha2003@gmail.com>
2023-08-28 13:39:58 +0530

    task list model

--- tasklist/models.py
+++ tasklist/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from authentication.models import User
 
 # Create your models here.
+
+class TaskList(models.Model):
+    user=models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    title=models.CharField(max_length=200)
+    desc=models.TextField()
+    created=models.DateTimeField(auto_now_add=True)
+    complete=models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering=['complete']
+    
